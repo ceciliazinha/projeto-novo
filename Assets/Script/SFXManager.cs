5850a46ae6728f62b82008cfc3e264d372e335ba
ceciliazinha <ceciliacandidosilva21@gmail.com>
2019-08-10 19:23:52 -0300

    Particulas de morte

--- Assets/Script/SFXManager.cs
+++ Assets/Script/SFXManager.cs
@@ -6,6 +6,7 @@ public class SFXManager : MonoBehaviour
 {
     public static SFXManager instance = null;
     public GameObject coinParticles;
+    public GameObject dieParticles;
 
     void Awake(){
         if(instance == null){
@@ -18,5 +19,10 @@ public class SFXManager : MonoBehaviour
         GameObject tape = GameObject.Find("Tape");
         particles.transform.SetParent(tape.transform);
     }
+    public void ShowDieParticles(GameObject obj) {
+        GameObject particles = Instantiate(dieParticles, obj.transform.position, Quaternion.identity);
+        GameObject tape = GameObject.Find("Tape");
+        particles.transform.SetParent(tape.transform);
+    } 
 }
 

c9a824eb6103417849ad1b10d550ce6e162f4b81
ceciliazinha <ceciliacandidosilva21@gmail.com>
2019-11-21 09:54:16 -0200

    Tela de advertencia

--- Assets/Script/NavigationManager.cs
+++ Assets/Script/NavigationManager.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class NavigationManager : MonoBehaviour
 {
-    public void LoadScene(string sceneName){
+    public static NavigationManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    public void LoadScene(string sceneName)
+    {
         SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Assets/Script/TimerText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerText : MonoBehaviour
+{
+    public Text textArea;
+    public string[] paragraphs;
+    public float waitLetter = 0.2f;
+    public float waitParagraph = 2f;
+
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        foreach (string paragraph in paragraphs)
+        {
+
+            for (int i = 0; i < paragraph.Length; i++)
+            {
+                textArea.text = paragraph.Substring(0, i);
+                // tocar som de tecla
+                yield return new WaitForSeconds(waitLetter);
+            }
+            yield return new WaitForSeconds(waitParagraph);
+        }
+        NavigationManager.instance.LoadScene("Main");
+    }
+}
